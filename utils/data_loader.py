0f7f445555bb30cd265d1ecf0dc6777f2179bec4
Himanshu Kumar <Himanshu.Kumar@Jaipuria.local>
2026-07-15 13:19:08 +0530

    Added debug code

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -3,10 +3,13 @@ import streamlit as st
 
 GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/1eziqvaIzCcnkp-VZhgrp15d48N3De_gi4Cks2MYblZg/gviz/tq?tqx=out:csv&sheet=Incentive_Owner"
 
-@st.cache_data(ttl=10)
+@st.cache_data(ttl=60)
 def load_data():
 
     df = pd.read_csv(GOOGLE_SHEET_URL, skiprows=1)
+
+    st.write("RAW DATA")
+    st.write(df.head(10))
 
     df.columns = [
         "S.No",
@@ -20,16 +23,7 @@ def load_data():
         "Amount"
     ]
 
-    numeric_cols = [
-        "NOIDA",
-        "LUCKNOW",
-        "JAIPUR",
-        "INDORE",
-        "TOTAL",
-        "Amount"
-    ]
-
-    for col in numeric_cols:
-        df[col] = pd.to_numeric(df[col], errors="coerce").fillna(0)
+    st.write("RENAMED DATA")
+    st.write(df.head(10))
 
     return df
